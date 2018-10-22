0ca0d69169454a02bec2a5f0802b0a3b852eaf94
wallyrion <kornienko1296@gmail.com>
2018-10-22 17:29:11 +0300

    [Version 1] Add Telegram token bot

---
+++ JuniorCuteBot/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuniorCuteBot
+{
+    public class Constants
+    {
+        public const string BotToken = "726291247:AAGBKReTs7pyxVQ516fXKKZe9HtW5Mu0Avk";
+    }
+}
--- JuniorCuteBot/Program.cs
+++ JuniorCuteBot/Program.cs
@@ -2,11 +2,31 @@
 
 namespace JuniorCuteBot
 {
+    using Telegram.Bot;
+    using Telegram.Bot.Args;
+
     class Program
     {
+        private static readonly TelegramBotClient Bot = new TelegramBotClient(Constants.BotToken);
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello from Alexey");
+            Bot.OnMessage += Bot_OnMessage;
+
+            Bot.StartReceiving();
+            Console.ReadLine();
+            Bot.StopReceiving();
+        }
+
+        private static void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+            {
+                if (e.Message.Text == "Hello")
+                    Bot.SendTextMessageAsync(e.Message.Chat.Id, $"Hello from JuniorBot");
+                else if (e.Message.Text == "Bye")
+                    Bot.SendTextMessageAsync(e.Message.Chat.Id, $"Bye from JuniorBot");
+            }
         }
     }
 }
